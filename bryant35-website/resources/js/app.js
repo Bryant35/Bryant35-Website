40180006794c34b8077f964c4bc5bb9238430937
Bat <banthony@student.ciputra.ac.id>
2024-06-22 22:36:53 +0700

    add Profile web

--- bryant35-website/resources/js/app.js
+++ bryant35-website/resources/js/app.js
@@ -1,8 +1,2 @@
 import './bootstrap';
 
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')  // Compile your JavaScript
-   .postCss('resources/css/app.css', 'public/css', [
-       require('tailwindcss'),  // Process with Tailwind CSS
-   ]);
